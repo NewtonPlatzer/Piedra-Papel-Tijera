e91450cf877f02306ff1e49ac7a28cbf2d9a960a
Ike Gomber <isaacbermude14@gmail.com>
2023-09-13 14:49:21 -0500

    Js Mínificado

--- script.js
+++ script.js
@@ -58,7 +58,6 @@ function playRound(computerChoice, humanChoice) {
     nameGanador.innerText = "Ganó la máquina";
     explicacionGanador.innerText = "El Papel envuelve a la Piedra";
     explicacionGanador.style.display = "block";
-
     return "computer";
   } else if (computerChoice == 3 && humanChoice == 2) {
     pUserSel.innerText = "Papel";
@@ -66,7 +65,6 @@ function playRound(computerChoice, humanChoice) {
     nameGanador.innerText = "Ganó la máquina";
     explicacionGanador.innerText = "Las Tijeras cortan el Papel";
     explicacionGanador.style.display = "block";
-
     return "computer";
   } else if (computerChoice == 3 && humanChoice == 1) {
     pUserSel.innerText = "Piedra";
@@ -74,7 +72,6 @@ function playRound(computerChoice, humanChoice) {
     nameGanador.innerText = "Ganaste!!!";
     explicacionGanador.innerText = "La Piedra destruye las Tijeras";
     explicacionGanador.style.display = "block";
-
     return "human";
   } else if (computerChoice == 1 && humanChoice == 2) {
     pUserSel.innerText = "Papel";
@@ -82,7 +79,6 @@ function playRound(computerChoice, humanChoice) {
     nameGanador.innerText = "Ganaste!!!";
     explicacionGanador.innerText = "El Papel envuelve la Piedra";
     explicacionGanador.style.display = "block";
-
     return "human";
   } else if (computerChoice == 2 && humanChoice == 3) {
     pUserSel.innerText = "Tijera";
@@ -90,11 +86,9 @@ function playRound(computerChoice, humanChoice) {
     nameGanador.innerText = "Ganaste!!!";
     explicacionGanador.innerText = "Las Tijeras cortan el Papel";
     explicacionGanador.style.display = "block";
-
     return "human";
   }
 }
-
 btnRock.addEventListener("click", function () {
   playRound(getComputerChoice(), humanChoice("rock"));
 });
@@ -104,28 +98,3 @@ btnPaper.addEventListener("click", function () {
 btnScissor.addEventListener("click", function () {
   playRound(getComputerChoice(), humanChoice("scissor"));
 });
-
-// function game() {
-//   let resultadosPartidas = [];
-//   for (let i = 0; i < 3; i++) {
-//     let resultado = playRound(getComputerChoice(), humanChoice());
-//     resultadosPartidas.push(resultado);
-//   }
-//   let resultadoComputerArray = [];
-//   let resultadoHumanArray = [];
-
-//   for (let i = 0; i < resultadosPartidas.length; i++) {
-//     if (resultadosPartidas[i] == "computer") {
-//       resultadoComputerArray.push(1);
-//     } else if (resultadosPartidas[i] == "human") {
-//       resultadoHumanArray.push(1);
-//     }
-//   }
-//   if (resultadoComputerArray.length < resultadoHumanArray.length) {
-//     console.log("Human win!!!");
-//   } else if (resultadoComputerArray.length > resultadoHumanArray.length) {
-//     console.log("Computer win!!!");
-//   } else {
-//     console.log("Draw!!!");
-//   }
-// }
